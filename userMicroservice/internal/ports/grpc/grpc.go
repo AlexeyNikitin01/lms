39f0f167f0784942b093c4762a35645bd8d6b90b
alnikitin <alnikitin@digital-spirit.ru>
2024-07-30 20:07:50 +0300

    feat: add docs for grpc

--- userMicroservice/internal/ports/grpc/grpc.go
+++ userMicroservice/internal/ports/grpc/grpc.go
@@ -8,6 +8,21 @@ import (
 	"lms-user/internal/app"
 )
 
+/*
+
+Транспортный слой - отвечает за взаимодействие серверной части приложение с внешним миром.
+
+Основными методы являются:
+	- getUser
+	- updateUser
+	- register
+	- authByLoginPassword
+	- interceptor for validation jwt token
+
+Авторизация осуществляется через валидацию jwt токена.
+
+*/
+
 type IServer interface {
 	UserServiceServer
 }
