cc1cb55e341c95e22328e76a76de303fd34913b9
alnikitin <alnikitin@digital-spirit.ru>
2024-08-13 12:32:14 +0300

    feat: add cors

--- userMicroservice/internal/ports/httpgin/server.go
+++ userMicroservice/internal/ports/httpgin/server.go
@@ -12,6 +12,7 @@ func Server(addr string, app app.IAppUser) *http.Server {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 
+	router.Use(corsMiddleware())
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 	router.Use(auth(app))
