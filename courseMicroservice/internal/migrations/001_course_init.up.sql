6a6afddc1f0262d900b018ea16644e2384bdbd29
alnikitin <alnikitin@digital-spirit.ru>
2025-01-19 05:51:44 +0300

    feat: migration

--- courseMicroservice/internal/migrations/001_course_init.up.sql
+++ courseMicroservice/internal/migrations/001_course_init.up.sql
@@ -1,39 +1,45 @@
-CREATE TABLE IF NOT EXISTS courses (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    photo_url TEXT NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz NOT NULL DEFAULT now(),
-    deleted_at timestamptz
+CREATE TABLE IF NOT EXISTS courses
+(
+    id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL,
+    description TEXT         NOT NULL,
+    photo_url   TEXT         NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS lectures (
+CREATE TABLE IF NOT EXISTS video_lectures
+(
+    id        BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    course_id BIGINT REFERENCES courses (id) NOT NULL,
+    url       TEXT                   NOT NULL
+);
+
+CREATE TABLE modules
+(
+    id        BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name      VARCHAR(255) NOT NULL,
+    course_id BIGINT NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES courses (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS lectures
+(
+    id        BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title     VARCHAR(100)                   NOT NULL,
+    module_id BIGINT REFERENCES modules (id) NOT NULL,
+    lecture   TEXT                           NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tests (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    course_id BIGINT REFERENCES courses(id) NOT NULL,
-    lecture TEXT NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz NOT NULL DEFAULT now(),
-    deleted_at timestamptz
+    name VARCHAR(100) NOT NULL,
+    lecture_id BIGINT NOT NULL,
+    FOREIGN KEY (lecture_id) REFERENCES lectures(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS questions (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    lecture_id BIGINT REFERENCES lectures(id) NOT NULL,
-    question VARCHAR(100) NOT NULL,
-    answer TEXT NOT NULL,
-    wrong_answer TEXT[] NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz NOT NULL DEFAULT now(),
-    deleted_at timestamptz
-);
-
-CREATE TABLE IF NOT EXISTS video_lectures (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    course_id BIGINT REFERENCES courses(id) NOT NULL,
-    url VARCHAR(300) NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz NOT NULL DEFAULT now(),
-    deleted_at timestamptz
+    id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    text       TEXT    NOT NULL,
+    is_correct BOOLEAN NOT NULL,
+    test_id    BIGINT  NOT NULL,
+    FOREIGN KEY (test_id) REFERENCES tests (id) ON DELETE CASCADE
 );
